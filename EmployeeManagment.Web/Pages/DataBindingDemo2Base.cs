0a111831551410f3b6b768c5c1fc7cf0c04420f9
Venko Trajkovski <infidel756@gmail.com>
2020-07-03 00:04:02 +0200

    Blazor component parameters and passing data from child to parent components and vice versa

---
+++ EmployeeManagment.Web/Pages/DataBindingDemo2Base.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagment.Web.Pages
+{
+    public class DataBindingDemo2Base : ComponentBase
+    {
+        public string Description { get; set; } = string.Empty;
+    }
+}
--- EmployeeManagment.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagment.Web/Pages/EmployeeListBase.cs
@@ -14,6 +14,8 @@ namespace EmployeeManagment.Web.Pages
     {
         [Inject] 
         public IEmployeeService EmployeeService { get; set; }
+
+        public bool ShowFooter { get; set; } = true;
         
         public IEnumerable<Employee> Employees { get; set; }
 
@@ -22,6 +24,19 @@ namespace EmployeeManagment.Web.Pages
             Employees = (await EmployeeService.GetEmployees()).ToList();
         }
 
+        public int SelectedEmployeesCount { get; set; } = 0;
+
+        protected void EmployeeSelectionChanged(bool isSelected)
+        {
+            if (isSelected)
+            {
+                SelectedEmployeesCount++;
+            }
+            else
+            {
+                SelectedEmployeesCount--;
+            }
+        }
         // private void LoadEmployees()
         // {
         //     Thread.Sleep(5000);
